66eb16dd28f5767c44ba2059bfcc4ce3af48824e
Pablo Vivera <pvivera@gmail.com>
2017-02-15 16:59:59 -0500

    Sample app and FluentSecurityRouteInvoker

---
+++ src/FluentSecurity.Nancy.Sample/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Nancy.ModelBinding;
+using Nancy.Owin;
+
+namespace FluentSecurity.Nancy.Sample
+{
+    public class Startup
+    {
+        private readonly IConfiguration config;
+
+        public Startup(IHostingEnvironment env)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseOwin(x => x.UseNancy(opt => opt.Bootstrapper = new Bootstrapper()));
+        }
+    }
+}
---
+++ src/FluentSecurity.Nancy/FluentSecurityRouteInvoker.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Responses.Negotiation;
+using Nancy.Routing;
+
+namespace FluentSecurity.Nancy
+{
+    public class FluentSecurityRouteInvoker : IRouteInvoker
+    {
+        private DefaultRouteInvoker _defaultRouteInvoker;
+        public FluentSecurityRouteInvoker(IResponseNegotiator negotiator)
+        {
+            _defaultRouteInvoker = new DefaultRouteInvoker(negotiator);
+        }
+
+        public async Task<Response> Invoke(Route route, CancellationToken cancellationToken,
+            DynamicDictionary parameters, NancyContext context)
+        {
+            return await _defaultRouteInvoker.Invoke(route, cancellationToken, parameters, context);
+        }
+    }
+}
