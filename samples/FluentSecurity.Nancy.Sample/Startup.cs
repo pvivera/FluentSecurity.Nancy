449831e500c45902aa10a07355b706aaf6d4fbf4
Pablo Vivera <pvivera@gmail.com>
2017-05-16 10:58:25 -0400

    Updated to Visual Studio 2017

---
+++ samples/FluentSecurity.Nancy.Sample/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Nancy.Owin;
+
+namespace FluentSecurity.Nancy.Sample
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseOwin(x => x.UseNancy(opt => opt.Bootstrapper = new Bootstrapper()));
+        }
+    }
+}
